0ac56c38a4cd1b048d9aef864cee234bd2095225
Alvaro Dormundo <adormundo@gmail.com>
2022-03-28 02:18:19 -0300

    Custom ConstraintViolationException

--- src/main/java/com/alrz/cursomc/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/alrz/cursomc/controllers/exceptions/ControllerExceptionHandler.java
@@ -4,10 +4,14 @@ import com.alrz.cursomc.services.exceptions.DataIntegrityException;
 import com.alrz.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -23,4 +27,14 @@ public class ControllerExceptionHandler {
         StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<StandardError> constraintViolation(ConstraintViolationException e, HttpServletRequest request) {
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Falha na validação dos dados",System.currentTimeMillis());
+        for(ConstraintViolation<?> x : e.getConstraintViolations()) {
+            err.addError(x.getPropertyPath().toString(), x.getMessageTemplate());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
 }
