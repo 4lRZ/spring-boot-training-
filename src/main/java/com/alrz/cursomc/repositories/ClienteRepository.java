fe36149eae8dfb1b02674a96cd7e7b7d7befea3c
Alvaro Dormundo <adormundo@gmail.com>
2022-03-29 06:34:39 -0300

    Custom validation: Email cannot be repeated

--- src/main/java/com/alrz/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/alrz/cursomc/repositories/ClienteRepository.java
@@ -3,7 +3,11 @@ package com.alrz.cursomc.repositories;
 import com.alrz.cursomc.entities.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
+
+    @Transactional(readOnly = true)
+    ClienteEntity findByEmail(String email);
 }
--- src/main/java/com/alrz/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/alrz/cursomc/services/validation/ClienteInsertValidator.java
@@ -2,8 +2,11 @@ package com.alrz.cursomc.services.validation;
 
 import com.alrz.cursomc.controllers.exceptions.FieldMessage;
 import com.alrz.cursomc.dto.ClienteNewDTO;
+import com.alrz.cursomc.entities.ClienteEntity;
 import com.alrz.cursomc.entities.enums.TipoCliente;
+import com.alrz.cursomc.repositories.ClienteRepository;
 import com.alrz.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -13,6 +16,8 @@ import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClientInsert, ClienteNewDTO> {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
     public void initialize(ClientInsert ann) {
@@ -29,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClientInsert,
 
         if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        ClienteEntity aux = clienteRepository.findByEmail(objDto.getEmail());
+        if (aux != null) {
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {
