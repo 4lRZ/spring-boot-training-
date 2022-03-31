966b8cd83df65083c83adad176dbe26cbc10a331
Alvaro Dormundo <adormundo@gmail.com>
2022-03-31 18:45:08 -0300

    Heroku Prod

---
+++ src/main/java/com/alrz/cursomc/config/ProdConfig.java
@@ -0,0 +1,26 @@
+package com.alrz.cursomc.config;
+
+import com.alrz.cursomc.services.DBService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("Prod")
+public class ProdConfig {
+
+    private final DBService dbService;
+
+    public ProdConfig(DBService dbService) {
+        this.dbService = dbService;
+    }
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+
+}
